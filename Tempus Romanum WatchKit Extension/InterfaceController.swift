0c724aa57b008206d44db2a39e11d6332eb13a84
Jason R Tibbetts <jason@tibbetts.net>
2018-09-26 19:47:43 -0400

    Initial Commit

---
+++ Tempus Romanum WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Tempus Romanum WatchKit Extension
+//
+//  Created by Jason R Tibbetts on 9/26/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Tempus Romanum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tempus Romanum
+//
+//  Created by Jason R Tibbetts on 9/26/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
