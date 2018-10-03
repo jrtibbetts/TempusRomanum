e6a734281abb0c7dade1af772cb921a9c2205595
Jason R Tibbetts <jason@tibbetts.net>
2018-10-02 23:49:30 -0400

    No ticket: Added conversion functions for turning latitude and longitude coordinates into a degrees-minutes-seconds string.

--- Tempus Romanum/Tevye.swift
+++ Tempus Romanum/Tevye.swift
@@ -2,11 +2,32 @@
 
 import CoreLocation
 import Foundation
+import PromiseKit
 
-open class Tevye: NSObject, CLLocationManagerDelegate {
+public final class Tevye: NSObject, CLLocationManagerDelegate {
 
-    open func sunriseSunset(for location: CLLocation) -> (Date, Date) {
-        return (Date(), Date())
+    public typealias SunriseSunset = (Date, Date)
+
+    // Example: http://api.usno.navy.mil/rstt/oneday?date=12/1/2016&coords=41.89N,12.48E&tz=1
+    fileprivate let urlPattern = "http://api.usno.navy.mil/rstt/oneday?date=%@&coords=%@,%@&tz=%@"
+    fileprivate var latitudeString: String = ""
+    fileprivate var longitudeString: String = ""
+    fileprivate var timezoneOffset: Int {
+        return Date()
+    }
+
+    public func sunriseSunset(for location: CLLocation) -> Promise<SunriseSunset> {
+        return Promise<SunriseSunset> { (seal) in
+            seal.fulfill((Date(), Date()))
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager,
+                                didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            latitudeString = String(double: location.coordinate.latitude)
+            let longitude = location.coordinate.longitude
+        }
     }
 
 }
