114f65c5b2521d03b11a72fb6163c4c8d20569cc
Jason R Tibbetts <jason@tibbetts.net>
2018-10-02 20:12:25 -0400

    No ticket: Added the Tevye sunrise/sunset calculator class.

---
+++ Tempus Romanum/Tevye.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+
+import CoreLocation
+import Foundation
+
+open class Tevye: NSObject, CLLocationManagerDelegate {
+
+    open func sunriseSunset(for location: CLLocation) -> (Date, Date) {
+    }
+
+}
