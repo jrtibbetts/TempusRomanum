707da9380f16e00469974b439f780a2886848ba3
Jason R Tibbetts <jason@tibbetts.net>
2018-10-08 22:13:58 -0400

    No ticket: Renamed the Tempus RomanumTests to TevyeTests.

--- Tempus RomanumTests/TevyeTests.swift
+++ Tempus RomanumTests/TevyeTests.swift
@@ -3,7 +3,7 @@
 import XCTest
 @testable import Tempus_Romanum
 
-class Tempus_RomanumTests: XCTestCase {
+class TevyeTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
