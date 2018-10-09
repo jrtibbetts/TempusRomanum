4f6280e1707da876e4147be0e2c96e1053dab133
Jason R Tibbetts <jason@tibbetts.net>
2018-10-08 22:07:10 -0400

    No ticket: FINALLY got the sunriseSunset() promise chain working.

--- Tempus Romanum/Tevye.swift
+++ Tempus Romanum/Tevye.swift
@@ -2,32 +2,89 @@
 
 import CoreLocation
 import Foundation
-import PromiseKit
+import PMKCoreLocation
+import PMKFoundation
+import Stylobate
 
 public final class Tevye: NSObject, CLLocationManagerDelegate {
 
     public typealias SunriseSunset = (Date, Date)
 
-    // Example: http://api.usno.navy.mil/rstt/oneday?date=12/1/2016&coords=41.89N,12.48E&tz=1
-    fileprivate let urlPattern = "http://api.usno.navy.mil/rstt/oneday?date=%@&coords=%@,%@&tz=%@"
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+
+        return formatter
+    }()
     fileprivate var latitudeString: String = ""
     fileprivate var longitudeString: String = ""
-    fileprivate var timezoneOffset: Int {
-        return Date()
-    }
+    fileprivate var timezoneOffset: Int = 0  // GMT
+    // Example: http://api.usno.navy.mil/rstt/oneday?date=12/1/2016&coords=41.89N,12.48E&tz=1
+    fileprivate let urlPattern = "http://api.usno.navy.mil/rstt/oneday?date=%@&coords=%@,%@&tz=%@"
 
-    public func sunriseSunset(for location: CLLocation) -> Promise<SunriseSunset> {
-        return Promise<SunriseSunset> { (seal) in
-            seal.fulfill((Date(), Date()))
+    public func sunriseSunset() -> Promise<SolarAndLunarData> {
+        return Promise<SolarAndLunarData>() { (seal) in
+            CLLocationManager.requestLocation().then {
+                URLSession.shared.dataTask(.promise, with: try self.request(for: $0[0].coordinate)!).validate()
+                }.done {
+                    seal.fulfill(try JSONDecoder().decode(SolarAndLunarData.self, from: $0.data))
+                }.catch { (error) in
+                    seal.reject(error)
+            }
         }
     }
 
-    public func locationManager(_ manager: CLLocationManager,
-                                didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            latitudeString = String(double: location.coordinate.latitude)
-            let longitude = location.coordinate.longitude
+    fileprivate func request(for coordinate: CLLocationCoordinate2D) throws -> URLRequest? {
+        let (latString, longString) = coordinate.degreesMinutesSeconds
+        let urlString = String(format: urlPattern, latString, longString)
+        if let url = URL(string: urlString) {
+            return URLRequest(url: url)
+        } else {
+            return nil
         }
+    }
+
+    public struct SolarAndLunarData: Codable {
+        var error: Bool
+        var apiversion: String
+        var year: Int
+        var month: Int
+        var day: Int
+        var dayofweek: String
+        var datechanged: Bool
+        var lon: Double
+        var lat: Double
+        var tz: Int
+
+        var sundata: [PhenomenonTime]
+        var moondata: [PhenomenonTime]
+
+        var closestphase: [Phase]
+        var fracillum: String
+        var curphase: String
+
+        public struct Phase: Codable {
+            var phase: String
+            var date: Date
+            var time: Date
+        }
+
+        public struct PhenomenonTime: Codable {
+
+            public enum Phenomenon: String, Codable {
+                case BC
+                case R
+                case U
+                case S
+                case EC
+            }
+
+            var phen: Phenomenon
+            var time: String
+
+        }
+
     }
 
 }
