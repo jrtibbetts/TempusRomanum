b6ccfc1477df7f4d4819720a99039e16316a2b69
Jason R Tibbetts <jason@tibbetts.net>
2018-10-12 00:55:36 -0400

    005: The main ViewController now has outlets for the time labels, and calls Tevye().sunriseSunset() to get that data.

--- Source/ViewController.swift
+++ Source/ViewController.swift
@@ -1,12 +1,32 @@
 //  Copyright © 2018 Poikile Creations. All rights reserved.
 
+import PromiseKit
+import Stylobate
 import UIKit
 
-class ViewController: UIViewController {
+public final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    // MARK: - Outlets
+
+    @IBOutlet fileprivate weak var romanTimeLabel: UILabel!
+    @IBOutlet fileprivate weak var modernTimeLabel: UILabel!
+    @IBOutlet fileprivate weak var clockView: UIView!
+
+    // MARK: - Private Properties
+
+    fileprivate let tevye = Tevye()
+
+    // MARK: - UIViewController
+
+    override public func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tevye.sunriseSunset().done { [weak self] (solarAndLunarData) in
+            let sunrise = solarAndLunarData.solarData.filter { $0.phenomenon == "U" }.first?.time
+            self?.romanTimeLabel?.text = sunrise
+            }.catch { (error) in
+                self.presentAlert(for: error, title: "Error")
+        }
     }
 
 
