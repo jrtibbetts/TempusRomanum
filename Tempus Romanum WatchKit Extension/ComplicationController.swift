46c67fc002f4098ba0e412aeda67e07e3e040cce
Jason R Tibbetts <jason@tibbetts.net>
2018-10-13 23:41:34 -0400

    No ticket: Removed boilerplate functions from ComplicationController.

--- Tempus Romanum WatchKit Extension/ComplicationController.swift
+++ Tempus Romanum WatchKit Extension/ComplicationController.swift
@@ -11,22 +11,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
                                           withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
         handler([.forward, .backward])
     }
-    
-    func getTimelineStartDate(for complication: CLKComplication,
-                              withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getTimelineEndDate(for complication: CLKComplication,
-                            withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getPrivacyBehavior(for complication: CLKComplication,
-                            withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
-        handler(.showOnLockScreen)
-    }
-    
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication,
@@ -34,30 +18,4 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         // Call the handler with the current timeline entry
         handler(nil)
     }
-    
-    func getTimelineEntries(for complication: CLKComplication,
-                            before date: Date,
-                            limit: Int,
-                            withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries prior to the given date
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication,
-                            after date: Date,
-                            limit: Int,
-                            withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
-    }
-    
-    // MARK: - Placeholder Templates
-    
-    func getLocalizableSampleTemplate(for complication: CLKComplication,
-                                      withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the
-        // results will be cached
-        handler(nil)
-    }
-    
 }
