3e74740645ec495fba9e6b2d126454ba1bd5d1a9
Jason R Tibbetts <jason@tibbetts.net>
2018-11-14 20:42:32 -0500

    Made SunriseSunset a full-fledged struct and factored it out of SolarAndLunarData.

--- Source/RomanClockView.swift
+++ Source/RomanClockView.swift
@@ -8,7 +8,7 @@ public final class RomanClockView: UIView {
 
     @IBInspectable public var clockBackgroundColor: UIColor? {
         didSet {
-            borderLayer.fillColor = (clockBackgroundColor ?? UIColor.white).cgColor
+            clockFace.fillColor = (clockBackgroundColor ?? UIColor.white).cgColor
             setNeedsDisplay()
         }
     }
@@ -21,14 +21,14 @@ public final class RomanClockView: UIView {
 
     @IBInspectable public var borderColor: UIColor? {
         didSet {
-            borderLayer.strokeColor = (borderColor ?? UIColor.black).cgColor
+            clockFace.strokeColor = (borderColor ?? UIColor.black).cgColor
             setNeedsDisplay()
         }
     }
 
     @IBInspectable public var borderThickness: NSDecimalNumber? {
         didSet {
-            borderLayer.lineWidth = CGFloat(borderThickness?.floatValue ?? 3.0)
+            clockFace.lineWidth = CGFloat(borderThickness?.floatValue ?? 3.0)
             setNeedsDisplay()
         }
     }
@@ -49,8 +49,7 @@ public final class RomanClockView: UIView {
 
     public override func awakeFromNib() {
         super.awakeFromNib()
-        layer.addSublayer(borderLayer)
-        layer.addSublayer(hourMarkersLayer)
+        layer.addSublayer(clockFace)
     }
 
     /*
@@ -67,33 +66,40 @@ public final class RomanClockView: UIView {
         }
     }
 
-    fileprivate lazy var borderLayer: CAShapeLayer = {
+    fileprivate lazy var clockFace: CAShapeLayer = {
         let layer = CAShapeLayer()
         layer.frame = self.bounds
 
         // We want a circle, so determine the largest circle that will fit in
-        // the bounds.
+        // the bounds, minus a margin for the numerals.
         let frameHeight = self.bounds.height
         let frameWidth = self.bounds.width
-        let minDimension = min(self.bounds.height, self.bounds.width)
+        let minDimension = (minimumDimension * 0.9)
         let circleSize = CGSize(width: minDimension, height: minDimension)
         let circleOrigin = CGPoint(x: (frameWidth / 2.0 - minDimension / 2.0),
                                    y: (frameHeight / 2.0 - minDimension / 2.0))
         layer.path = CGPath(ellipseIn: CGRect(origin: circleOrigin, size: circleSize), transform: nil)
 
-        [1..<24].forEach { (i) in
-
+        if let solarData = sunriseSunset {
+            (1...24).forEach { (i) in
+                if i % 6 == 0 {
+                    let textLayer = CATextLayer()
+                    textLayer.frame = CGRect(origin: circleOrigin, size: circleSize.applying(CGAffineTransform(translationX: 30.0, y: 30.0)))
+                    textLayer.setAffineTransform(CGAffineTransform(rotationAngle: (CGFloat(i) / 12.0) * CGFloat.pi + (CGFloat.pi / 4)))
+                    textLayer.string = "\(i)"
+                    textLayer.fontSize = 16.0
+                    textLayer.foregroundColor = UIColor.black.cgColor
+                    textLayer.backgroundColor = UIColor.clear.cgColor
+                    layer.addSublayer(textLayer)
+                }
+            }
         }
 
         return layer
     }()
 
-    fileprivate lazy var hourMarkersLayer: CAReplicatorLayer = {
-        let layer = CAReplicatorLayer()
-        layer.instanceCount = 24
-
-        let transform = CATransform3DMakeRotation(CGFloat(2 * Float.pi / 24.0), 3.0, 40, 0.0)
-        return layer
-    }()
+    fileprivate var minimumDimension: CGFloat {
+        return min(self.bounds.height, self.bounds.width)
+    }
 
 }
--- Source/ViewController.swift
+++ Source/ViewController.swift
@@ -23,10 +23,10 @@ public final class ViewController: UIViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-        tevye.sunriseSunset().done { [weak self] (solarAndLunarData) in
-            self?.modernSunriseLabel?.text = solarAndLunarData.sunriseString
-            self?.modernSunsetLabel?.text = solarAndLunarData.sunsetString
-            self?.clockView?.sunriseSunset = (solarAndLunarData.sunrise, solarAndLunarData.sunset)
+        tevye.sunriseSunset().done { [weak self] (sunriseSunset) in
+            self?.modernSunriseLabel?.text = sunriseSunset.sunriseString
+            self?.modernSunsetLabel?.text = sunriseSunset.sunsetString
+            self?.clockView?.sunriseSunset = sunriseSunset
             }.catch { (error) in
                 self.presentAlert(for: error, title: "Error")
         }
