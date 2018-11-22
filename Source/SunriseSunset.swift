52a8ae16a1d51aa1add981c67760c4d7a3aa8959
Jason R Tibbetts <jason@tibbetts.net>
2018-11-21 23:36:00 -0500

    009: Removed the standalone SunriseSunset file. It will probably rise again.

--- Source/SunriseSunset.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SunriseSunset.swift
-//  Tempus Romanum
-//
-//  Created by Jason R Tibbetts on 11/14/18.
-//  Copyright © 2018 Poikile Creations. All rights reserved.
-//
-
-import Foundation
