c93b0e165fd01d25b29909cebd54993a3dce833f
Jason R Tibbetts <jason@tibbetts.net>
2018-11-22 09:02:15 -0500

    009: Factored out the HourLinesLayer and Date rotationAngle from RomanClockView.

--- Source/Views/HourLinesLayer.swift
+++ Source/Views/HourLinesLayer.swift
@@ -1,9 +1,36 @@
-//
-//  HourLinesLayer.swift
-//  Tempus Romanum
-//
-//  Created by Jason R Tibbetts on 11/22/18.
 //  Copyright © 2018 Poikile Creations. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+/// A `CALayer` that draws lines from its center to points along the perimeter
+/// of a circle that just fills the frame, where the points correspond to times
+/// relative to midnight on the date.
+open class HourLinesLayer: CAShapeLayer {
+
+    public convenience init(rect: CGRect,
+                            dates: [Date]) {
+        self.init()
+
+        // Set up the geometry.
+        frame = rect
+        let center = CGPoint(x: rect.width / 2.0, y: rect.height / 2.0)
+        let minimumDimension = min(rect.width, rect.height)
+        let radius = minimumDimension / 2.0
+
+        // Create the path.
+        let path = UIBezierPath()
+        path.lineWidth = 2.0
+
+        dates.forEach { (date) in
+            let angle = date.rotationAngle  // relative to midnight of that
+                                            // date.
+            path.move(to: center)
+            let borderPoint = CGPoint(x: center.x + (radius * cos(angle)),
+                                      y: center.y + (radius * sin(angle)))
+            path.addLine(to: borderPoint)
+        }
+
+        self.path = path.cgPath
+    }
+
+}
