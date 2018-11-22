53488b963280b515533efb4543415e7914bdf379
Jason R Tibbetts <jason@tibbetts.net>
2018-11-22 18:47:55 -0500

    No ticket: More .travis.yml crap.

---
+++ Source/Views/DaylightLayer.swift
@@ -0,0 +1,9 @@
+//
+//  DaylightLayer.swift
+//  Tempus Romanum
+//
+//  Created by Jason R Tibbetts on 11/22/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+import Foundation
