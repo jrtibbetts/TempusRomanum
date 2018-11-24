5a822c494ce29359f054eb9be8166b4b3c666bb7
Jason R Tibbetts <jason@tibbetts.net>
2018-11-23 19:49:57 -0500

    009: Factored out clock components into ClockLayer, DaylightLayer, and ModernHourMarksLayer classes.

---
+++ Source/Views/ClockLayer.swift
@@ -0,0 +1,24 @@
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+
+import UIKit
+
+/// Base class for shape layers that display components of a round, analog
+/// clock face.
+open class ClockLayer: CAShapeLayer {
+
+    /// Get the exact center of the layer's `bounds`.
+    open var center: CGPoint {
+        return CGPoint(x: bounds.width / 2.0, y: bounds.height / 2.0)
+    }
+
+    /// Get the maximum radius of a circle that will fit in the bounds.
+    open var radius: CGFloat {
+        return diameter / 2.0
+    }
+
+    /// Get the diameter of the largest circle that will fit in the bounds.
+    open var diameter: CGFloat {
+        return min(bounds.width, bounds.height)
+    }
+
+}
--- Source/Views/DaylightLayer.swift
+++ Source/Views/DaylightLayer.swift
@@ -1,9 +1,35 @@
-//
-//  DaylightLayer.swift
-//  Tempus Romanum
-//
-//  Created by Jason R Tibbetts on 11/22/18.
 //  Copyright © 2018 Poikile Creations. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+class DaylightLayer: ClockLayer {
+
+    fileprivate var hourLinesLayer: HourLinesLayer?
+
+    public var hoursAndEndTime: (hours: [Date], end: Date)? {
+        didSet {
+            guard let hours = hoursAndEndTime?.hours,
+                let end = hoursAndEndTime?.end else {
+                return
+            }
+
+            let startAngle = hours[0].rotationAngle
+            let sunsetAngle = end.rotationAngle
+            let daylightPath = UIBezierPath(sliceCenter: center,
+                                        radius: radius,
+                                        startAngle: startAngle,
+                                        endAngle: sunsetAngle)
+
+            path = daylightPath.cgPath
+            fillColor = UIColor.yellow.cgColor
+
+            hourLinesLayer?.removeFromSuperlayer()
+            hourLinesLayer = HourLinesLayer(hours: hours)
+            hourLinesLayer?.lineWidth = 1.0
+            hourLinesLayer?.strokeColor = UIColor.lightGray.cgColor
+            hourLinesLayer?.frame = bounds
+            addSublayer(hourLinesLayer!)
+        }
+    }
+
+}
