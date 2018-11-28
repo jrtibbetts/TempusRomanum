752b2dacc8d6b4169fb015b2a1ef4c063bcd6e6e
Jason R Tibbetts <jason@tibbetts.net>
2018-11-28 16:31:14 -0500

    010: Added a twelve-hour time formatter to the ViewController.

--- Source/ViewController.swift
+++ Source/ViewController.swift
@@ -18,17 +18,25 @@ public final class ViewController: UIViewController {
 
     fileprivate let sunriseSunsetProvider = MockSunriseSunsetProvider()
 
+    fileprivate var timeFormatter: DateFormatter!
+    
     /// A `DateFormatter` for getting displaying 24-hour time for `Date`s,
     /// like `08:00` (as opposed to true military time, which doesn't use
     /// colons, like `0800`). The date information of the date is not displayed.
-    fileprivate let timeFormatter = DateFormatter() <~ {
+    fileprivate let twentyFourHourTimeFormatter = DateFormatter() <~ {
         $0.dateFormat = "HH:mm"
+    }
+    
+    fileprivate let twelveHourTimeFormatter = DateFormatter() <~ {
+        $0.dateFormat = "hh:mm a"
     }
 
     // MARK: - UIViewController
 
     override public func viewDidLoad() {
         super.viewDidLoad()
+        
+        timeFormatter = twelveHourTimeFormatter
 
         sunriseSunsetProvider.sunriseSunset().done { [weak self] (sunriseSunset) in
             guard let self = self else { return }
