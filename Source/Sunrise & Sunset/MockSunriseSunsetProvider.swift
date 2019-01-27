c011296de476900aab177612d6ccc2aa08436044
Jason R Tibbetts <jason@tibbetts.net>
2019-01-27 16:41:34 -0500

    011: Renamed MockSunriseSunset to SimpleSunriseSunset.

--- Source/Sunrise & Sunset/MockSunriseSunsetProvider.swift
+++ Source/Sunrise & Sunset/MockSunriseSunsetProvider.swift
@@ -3,19 +3,19 @@
 import PromiseKit
 import UIKit
 
-open class MockSunriseSunsetProvider: SunriseSunsetProvider {
+struct SimpleSunriseSunset: SunriseSunset {
+    var sunrise: Date
+    var sunset: Date
+}
 
-    struct MockSunriseSunset: SunriseSunset {
-        var sunrise: Date
-        var sunset: Date
-    }
+open class MockSunriseSunsetProvider: SunriseSunsetProvider {
 
     public func sunriseSunset() -> Promise<SunriseSunset> {
         return Promise<SunriseSunset> { (promise) in
             let startOfToday = Calendar.current.startOfDay(for: Date())
             let sunrise = startOfToday.addingTimeInterval(5.3 * 60.0 * 60.0)
             let sunset = startOfToday.addingTimeInterval(19.12 * 60.0 * 60.0)
-            promise.fulfill(MockSunriseSunset(sunrise: sunrise, sunset: sunset))
+            promise.fulfill(SimpleSunriseSunset(sunrise: sunrise, sunset: sunset))
         }
     }
     
