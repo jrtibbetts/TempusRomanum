5cee22fef9dae8aa0ac145b4eb3d8e546821045b
Jason R Tibbetts <jason@tibbetts.net>
2019-01-27 09:26:47 -0500

    No ticket: Moved ClockSubfaceLayer into its own file.

---
+++ Source/Views/ClockSubfaceLayer.swift
@@ -0,0 +1,60 @@
+//  Copyright © 2019 Poikile Creations. All rights reserved.
+
+import UIKit
+
+/// Base class for the daytime and nighttime clock subfaces. I'm sure that
+/// there's an official watchmaking term for the daytime and nighttime sections,
+/// but I couldn't find one!
+class ClockSubfaceLayer: ClockLayer {
+
+    // MARK: - Public Properties
+
+    /// The layer that draws lines for each hour in this subface.
+    var hourLinesLayer = HourLinesLayer()
+
+    /// The astronomical information.
+    var sunriseSunset: SunriseSunset?
+
+    // MARK: - Initialization
+
+    convenience init(sunriseSunset: SunriseSunset) {
+        self.init()
+        self.sunriseSunset = sunriseSunset
+    }
+
+    override init() {
+        super.init()
+
+        hourLinesLayer.strokeColor = UIColor(named: "Hour Marks")?.cgColor
+        hourLinesLayer.lineWidth = 1.0
+        addSublayer(hourLinesLayer)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override init(layer: Any) {
+        super.init(layer: layer)    // This prevents crashes when rotating.
+    }
+
+    // MARK: - CALayer
+
+    override func layoutSublayers() {
+        super.layoutSublayers()
+
+        hourLinesLayer.frame = bounds
+        hourLinesLayer.layoutSublayers()
+
+        updatePath()
+    }
+
+    // MARK: - Other Functions
+
+    /// Recalculate the Bézier path. This implementation does nothing, so
+    /// subclasses should override it with their own implementations and not
+    /// bother calling `super.updatePath()`.
+    func updatePath() {
+    }
+
+}
