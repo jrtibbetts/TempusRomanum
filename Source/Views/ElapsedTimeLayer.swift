2d9cae4c3780793ba478026442c53760fda47c31
Jason R Tibbetts <jason@tibbetts.net>
2019-01-27 22:51:16 -0500

    No ticket: Got rid of the ElapsedTimeLayer's initializers.

--- Source/Views/ElapsedTimeLayer.swift
+++ Source/Views/ElapsedTimeLayer.swift
@@ -10,27 +10,6 @@ class ElapsedTimeLayer: ClockLayer {
     /// The lowest point on the circle.
     let midnightAngle = 0.5 * CGFloat.pi
 
-    // MARK: - Initializers
-
-    override init() {
-        super.init()
-        commonInit()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        commonInit()
-    }
-
-    override init(layer: Any) {
-        super.init(layer: layer)
-        commonInit()
-    }
-
-    private func commonInit() {
-        fillColor = UIColor(named: "Elapsed Time")?.cgColor
-    }
-
     // MARK: - Functions
 
     /// Set the semicircular path from midnight until the specified time.
@@ -38,6 +17,7 @@ class ElapsedTimeLayer: ClockLayer {
     /// - parameter date: The end time of the shaded area. By default, this is
     ///                   the exact time when this function was called.
     func update(date: Date = Date()) {
+        fillColor = UIColor(named: "Elapsed Time")?.cgColor
         let endAngle = date.rotationAngle
 
         path = UIBezierPath(sliceCenter: boundsCenter,  // from ClockLayer
