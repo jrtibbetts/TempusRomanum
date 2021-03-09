787e630e31efeeebddc07c15c63e6f47f74de1d6
Jason R Tibbetts <jason@tibbetts.net>
2021-03-08 23:47:43 -0500

    #31: Updated some unit tests.

--- Tests/Views/Date+TrigonometryTests.swift
+++ Tests/Views/Date+TrigonometryTests.swift
@@ -12,6 +12,8 @@ class Date_TrigonometryTests: XCTestCase {
         return formatter
     }()
 
+    // MARK: - Minute scale
+
     func testTwoOClockMinutesAngleIs270Degrees() {
         let time = dateFormatter.date(from: "2:00 AM")!
         XCTAssertEqual(time.minutesAngle.degrees, 270.0, accuracy: 0.001)
@@ -32,25 +34,49 @@ class Date_TrigonometryTests: XCTestCase {
         XCTAssertEqual(time.minutesAngle.degrees, 180.0, accuracy: 0.001)
     }
 
-    func testMidnightRotationAngleOk() {
+    // MARK: - 12-hour scale
+
+    func testMidnight12HourRotationAngleOk() {
+        let midnight = dateFormatter.date(from: "12:00 AM")!
+        XCTAssertEqual(midnight.hour12RotationAngle.degrees, 270.0, accuracy: 0.001) // TR
+    }
+
+    func test6am12HourRotationAngleOk() {
+        let sixAm = dateFormatter.date(from: "6:00 AM")!
+        XCTAssertEqual(sixAm.hour12RotationAngle.degrees, 90.0, accuracy: 0.001) // TR
+    }
+
+    func testNoon12HourRotationAngleOk() {
+        let noon = dateFormatter.date(from: "12:00 PM")!
+        XCTAssertEqual(noon.hour12RotationAngle.degrees, 270.0, accuracy: 0.001) // TR
+    }
+
+    func test6pm12HourRotationAngleOk() {
+        let sixPm = dateFormatter.date(from: "6:00 PM")!
+        XCTAssertEqual(sixPm.hour12RotationAngle.degrees, 90.0, accuracy: 0.001) // TR
+    }
+
+    // MARK: - 24-hour scale
+
+    func testMidnight24HourRotationAngleOk() {
         let midnight = dateFormatter.date(from: "12:00 AM")!
         XCTAssertEqual(midnight.rotationAngle, (CGFloat.pi / 2.0), accuracy: 0.001) // Stylobate
         XCTAssertEqual(midnight.hour24RotationAngle.degrees, 270.0, accuracy: 0.001) // TR
     }
 
-    func test6amRotationAngleOk() {
+    func test6am24HourRotationAngleOk() {
         let sixAm = dateFormatter.date(from: "6:00 AM")!
         XCTAssertEqual(sixAm.rotationAngle, CGFloat.pi, accuracy: 0.001) // Stylobate
         XCTAssertEqual(sixAm.hour24RotationAngle.degrees, 0.0, accuracy: 0.001) // TR
     }
 
-    func testNoonRotationAngleOk() {
+    func testNoon24HourRotationAngleOk() {
         let noon = dateFormatter.date(from: "12:00 PM")!
         XCTAssertEqual(noon.rotationAngle, (1.5 * CGFloat.pi), accuracy: 0.001) // Stylobate
         XCTAssertEqual(noon.hour24RotationAngle.degrees, 90.0, accuracy: 0.001) // TR
     }
 
-    func test6pmRotationAngleOk() {
+    func test6pm24HourRotationAngleOk() {
         let sixPm = dateFormatter.date(from: "6:00 PM")!
         XCTAssertEqual(sixPm.rotationAngle, 2.0 * CGFloat.pi, accuracy: 0.001) // Stylobate
         XCTAssertEqual(sixPm.hour24RotationAngle.degrees, 180.0, accuracy: 0.001) // TR
--- Tests/Views/DaylightLayerTests.swift
+++ Tests/Views/DaylightLayerTests.swift
@@ -5,22 +5,28 @@ import XCTest
 
 class DaylightLayerTests: XCTestCase {
 
+    var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .short
+
+        return dateFormatter
+    }()
+
     func testSetSunriseSunsetUpdatesPath() {
         let layer = DaylightLayer()
         layer.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
         let midnight = Calendar.current.startOfDay(for: Date())
-        let sunset = midnight.addingTimeInterval(12 * 60 * 1000)
-        var sunriseSunset = SimpleSunriseSunset(sunrise: midnight, sunset: sunset)
-        layer.sunriseSunset = sunriseSunset
+        let sixAM = midnight.addingTimeInterval(6 * 60 * 60)
+        let sixPM = midnight.addingTimeInterval(18 * 60 * 60)
+        layer.sunriseSunset = SimpleSunriseSunset(sunrise: sixAM, sunset: sixPM)
         layer.layoutIfNeeded()  // since we're not running a UI test, force the layout
-        XCTAssertTrue(layer.path!.contains(CGPoint(x: 100, y: 200)))
-        XCTAssertFalse(layer.path!.contains(CGPoint(x: 300, y: 200)))
+        XCTAssertTrue(layer.path!.contains(CGPoint(x: 50, y: 50)))
+        XCTAssertFalse(layer.path!.contains(CGPoint(x: 350, y: 350)))
 
-        sunriseSunset = SimpleSunriseSunset(sunrise: sunset, sunset: midnight)
-        layer.sunriseSunset = sunriseSunset
+        layer.sunriseSunset = SimpleSunriseSunset(sunrise: sixPM, sunset: sixAM)
         layer.layoutIfNeeded()
-        XCTAssertFalse(layer.path!.contains(CGPoint(x: 100, y: 200)))
-        XCTAssertTrue(layer.path!.contains(CGPoint(x: 300, y: 200)))
+        XCTAssertFalse(layer.path!.contains(CGPoint(x: 50, y: 50)))
+        XCTAssertTrue(layer.path!.contains(CGPoint(x: 350, y: 350)))
     }
 
 }
